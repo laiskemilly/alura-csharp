0f3d323e82b3d445d95a91e591d67f23c4a19a73
Lais Kemilly <lais.kemilly@flow42.com.br>
2026-02-05 12:13:20 -0300

    ex alura - propriedades

---
+++ Alura-propriedades/Alura-propriedades/Product.cs
@@ -0,0 +1,41 @@
+﻿class Produto()
+{
+    public string Nome { get; set; }
+    public string Marca { get; set; }
+    private double valor;
+    private int estoque;
+
+    public double Valor
+    {
+        get => valor;
+        set
+        {
+            if (value > 0)
+            {
+                valor = value;
+            }
+            else
+            {
+                Console.WriteLine("Erro no valor");
+            }
+        }
+    }
+
+    public int Estoque
+    {
+        get => estoque;
+        set
+        {
+            if (value > 0)
+            {
+                estoque = value;
+            }
+            else
+            {
+                estoque = 0;
+            }
+        }
+    }
+
+    public string InfoProduto => $"INFORMAÇÕES DO PRODUTO \n{this.Nome} {this.Marca} {this.valor}";
+}
---
+++ Alura-propriedades/Alura-propriedades/Program.cs
@@ -0,0 +1,14 @@
+﻿Produto produto1 = new Produto();
+produto1.Nome = "Notebook";
+produto1.Marca = "Dell";
+produto1.Valor = 4000;
+produto1.Estoque = 23;
+
+Produto produto2 = new Produto();
+produto2.Nome = "Ipad";
+produto2.Marca = "Apple";
+produto2.Valor = 5000;
+produto2.Estoque = 0;
+
+Console.WriteLine(produto1.InfoProduto);
+Console.WriteLine("\n" + produto2.InfoProduto);
