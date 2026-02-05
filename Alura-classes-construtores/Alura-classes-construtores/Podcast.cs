e15ee0b11591b2ebd816167d925bcec4d58c9cea
Lais Kemilly <lais.kemilly@flow42.com.br>
2026-02-05 19:16:37 -0300

    ex - alura classes

---
+++ Alura-classes-construtores/Alura-classes-construtores/Podcast.cs
@@ -0,0 +1,28 @@
+﻿class Podcast
+{
+    private List<Episodio> episodios = new();
+    public Podcast(string nome, string host)
+    {
+        Nome = nome;
+        Host = host;
+    }
+
+    public string Nome {  get;  }
+    public string Host { get;  }
+    public int TotalEpisodios => episodios.Count;
+
+    public void AddEpisodio(Episodio episodio)
+    {
+        episodios.Add(episodio);
+    }
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Podcast {Nome} apresentado por {Host}\n");
+        foreach (Episodio episodio in episodios.OrderBy(episodios => episodios.Ordem))
+        {
+            Console.WriteLine(episodio.Resumo);
+
+        }
+        Console.WriteLine($"\nEsse podcast possui {TotalEpisodios} episódios.");
+    }
+}
---
+++ Alura-classes-construtores/Alura-classes-construtores/Program.cs
@@ -0,0 +1,17 @@
+﻿Episodio ep1 = new(2, "Técnicas de aprendizado", 45);
+ep1.AddConvidados("Ana");
+ep1.AddConvidados("Maria");
+
+Episodio ep2 = new(1, "Técnicas de leitura", 30);
+ep2.AddConvidados("Laís");
+ep2.AddConvidados("Lorenna");
+
+Episodio ep3 = new(3, "Técnicas de foco", 50);
+ep3.AddConvidados("Maria");
+ep3.AddConvidados("Andre");
+
+Podcast podcast = new("Podcast especial", "Laís");
+podcast.AddEpisodio(ep1);
+podcast.AddEpisodio(ep2);
+podcast.AddEpisodio(ep3);
+podcast.ExibirDetalhes();
